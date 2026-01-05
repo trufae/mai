faf66f0932ba50e6b3766a358939fdadfa739837
pancake <pancake@nopcode.org>
2026-01-05 01:05:57 +0100

    Fix crash in the repl with ^R

--- src/repl/repl_core.go
+++ src/repl/repl_core.go
@@ -456,9 +456,19 @@ func (r *REPL) interruptResponse() {
 
 func (r *REPL) setupSignalHandler() {
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
-		<-c
+		sig := <-c
+		// Handle SIGQUIT gracefully
+		if sig == syscall.SIGQUIT {
+			if r.readline != nil {
+				fmt.Print("^\n")
+				r.readline.SetContent("")
+			}
+			r.interruptResponse()
+			r.setupSignalHandler()
+			return
+		}
 		// Reset the current editing line when interrupted
 		if r.readline != nil {
 			fmt.Print("^C\n")
